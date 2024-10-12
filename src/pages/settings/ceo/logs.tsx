29dc634266a9ec436906fc33444ca740a05c2f37
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-10-12 18:54:40 +0500

    ew

--- src/pages/settings/ceo/logs.tsx
+++ src/pages/settings/ceo/logs.tsx
@@ -1,4 +1,6 @@
+import { Pagination } from '@mui/material'
 import React, { useEffect, useState } from 'react'
+import EmptyContent from 'src/@core/components/empty-content'
 import { formatPhoneNumber } from 'src/@core/components/phone-input/format-phone-number'
 import useResponsive from 'src/@core/hooks/useResponsive'
 import api from 'src/@core/utils/api'
@@ -15,77 +17,96 @@ export default function Logs() {
       date: string
     }[]
   >([])
+  const [page, setPage] = useState(0)
+  const [count, setCount] = useState(0)
 
   const { isMobile } = useResponsive()
 
-  const getHistory = async () => {
+  const getHistory = async (p: number) => {
+    scrollTo(0, 0)
+    setPage(p)
     try {
-      const resp = await api.get(`auth/student/logs/`)
+      const resp = await api.get(`auth/student/logs/?page=${p}`)
       setData(resp.data?.results)
+      setCount(resp.data?.count)
     } catch (err: any) {}
   }
 
   useEffect(() => {
-    getHistory()
+    getHistory(1)
   }, [])
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: isMobile ? 10 : 40,
-        marginTop: isMobile ? 0 : 30,
-        alignItems: 'flex-start'
-      }}
-    >
-      {data.map(el => (
-        <div
-          style={{
-            backgroundColor: 'white',
-            padding: '20px',
-            borderRadius: '14px',
-            position: 'relative',
-            display: 'flex',
-            gap: isMobile ? 10 : '30px',
-            flexDirection: isMobile ? 'column' : 'row',
-            maxWidth: '600px',
-            width: '100%'
-          }}
-        >
-          <p
+    <div>
+      {data.length > 0 ? '' : <EmptyContent />}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: isMobile ? 10 : 40,
+          marginTop: isMobile ? 0 : 30,
+          alignItems: 'flex-start'
+        }}
+      >
+        {data.map(el => (
+          <div
             style={{
-              position: isMobile ? 'relative' : 'absolute',
-              top: isMobile ? 0 : '-30px',
               backgroundColor: 'white',
-              padding: isMobile ? 0 : '6px 20px',
-              left: 0,
-              borderTopLeftRadius: 10,
-              borderTopRightRadius: 10,
-              fontSize: '14px',
-              margin: '0 0 4px'
+              padding: '20px',
+              borderRadius: '14px',
+              position: 'relative',
+              display: 'flex',
+              gap: isMobile ? 10 : '30px',
+              flexDirection: isMobile ? 'column' : 'row',
+              maxWidth: '600px',
+              width: '100%'
             }}
           >
-            {el.title}. XODIM: {formatPhoneNumber(el?.admin_phone)} {el?.admin_name}
-          </p>
+            <p
+              style={{
+                position: isMobile ? 'relative' : 'absolute',
+                top: isMobile ? 0 : '-30px',
+                backgroundColor: 'white',
+                padding: isMobile ? 0 : '6px 20px',
+                left: 0,
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+                fontSize: '14px',
+                margin: '0 0 4px'
+              }}
+            >
+              {el.title}. XODIM: {formatPhoneNumber(el?.admin_phone)} {el?.admin_name}
+            </p>
 
-          <div>
-            <p style={{ fontSize: '14px', margin: '0 0 4px' }}>{el.user_name}</p>
+            <div>
+              <p style={{ fontSize: '14px', margin: '0 0 4px' }}>{el.user_name}</p>
 
-            <p style={{ fontSize: '14px', margin: '0 0 4px' }}>{el.user_phone}</p>
+              <p style={{ fontSize: '14px', margin: '0 0 4px' }}>{el.user_phone}</p>
 
-            <p style={{ fontSize: '14px', margin: '0 0 4px' }}>{el.date}</p>
+              <p style={{ fontSize: '14px', margin: '0 0 4px' }}>{el.date}</p>
+            </div>
+
+            <div>
+              {el?.text.split('\n').map((line, index) => (
+                <p style={{ fontSize: '14px', margin: '0 0 4px' }} key={index}>
+                  {line}
+                </p>
+              ))}
+            </div>
           </div>
-
-          <div>
-            {el?.text.split('\n').map((line, index) => (
-              <p style={{ fontSize: '14px', margin: '0 0 4px' }} key={index}>
-                {line}
-              </p>
-            ))}
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      {Math.ceil(count / 10) > 1 && data?.length && (
+        <Pagination
+          style={{ marginTop: '30px' }}
+          defaultPage={page || 1}
+          count={Math.ceil(count / 10)}
+          variant='outlined'
+          shape='rounded'
+          page={page}
+          onChange={(_: any, page) => getHistory(page)}
+        />
+      )}
     </div>
   )
 }
