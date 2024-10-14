d878c0d8eac8b17c81be38ca158bb8a8bc751511
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-10-14 18:43:58 +0500

    dasd

--- src/navigation/horizontal/index.ts
+++ src/navigation/horizontal/index.ts
@@ -85,10 +85,10 @@ const Navigation = (t: any): HorizontalNavItemsType => {
               title: t("To'lovlar"),
               path: '/settings/logs/payment-history'
             },
-            // {
-            //   title: t('Bot xabarnoma'),
-            //   path: '/settings/logs/bot-notification'
-            // }
+            {
+              title: t('Bot xabarnoma'),
+              path: '#'
+            }
           ]
         },
         {
@@ -119,11 +119,90 @@ const Navigation = (t: any): HorizontalNavItemsType => {
     // }
   ]
 
+
+  const adminItems = [
+    {
+      title: t('Bosh sahifa'),
+      icon: 'clarity:home-solid',
+      path: '/dashboard'
+    },
+    {
+      title: t('Lidlar'),
+      icon: 'mdi:receipt-text-edit-outline',
+      path: '/lids'
+    },
+    {
+      title: t('Mentorlar'),
+      icon: 'fa6-solid:chalkboard-user',
+      path: '/mentors'
+    },
+    {
+      title: t('Guruhlar'),
+      icon: 'uis:layer-group',
+      path: '/groups'
+    },
+    {
+      title: t("O'quvchilar"),
+      icon: 'mdi:account-student',
+      path: '/students'
+    },
+    {
+      title: t('Sozlamalar'),
+      icon: 'mdi:settings',
+      slug: 'settings',
+      children: [
+        {
+          title: 'SMS Sozlamalari',
+          path: '/settings/sms'
+        },
+        {
+          title: 'Ofis',
+          children: [
+            {
+              title: 'Kurslar',
+              path: '/settings/office/courses'
+            },
+            {
+              title: 'Xonalar',
+              path: '/settings/office/rooms'
+            },
+            {
+              title: 'Dam olish kunlari',
+              path: '/settings/office/free-days'
+            }
+            // {
+            //   title: 'Arxiv',
+            //   path: '/settings/office/archive'
+            // },
+            // {
+            //   title: 'Tark etgan talabalar',
+            //   path: '/settings/office/lefted-students'
+            // }
+          ]
+        },
+        {
+          title: 'Formalar',
+          path: '/settings/forms/'
+        }
+      ]
+    },
+    {
+      title: t('Hisobotlar'),
+      icon: 'tabler:report',
+      children: [
+        {
+          title: "O'quvchilar to'lovi",
+          path: '/reports/student-payment'
+        }
+      ]
+    }
+  ]
+
   return user?.role.includes('ceo')
     ? items
     : user?.role.includes('casher')
       ? items.filter(el => el.path == '/finance')
-      : items.filter(el => el.path !== '/finance')
+      : adminItems
 }
 
 export default Navigation
