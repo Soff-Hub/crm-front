cfc27a2b1c6f437ede9f74422db45cc8d7e9ab28
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-10-28 12:22:47 +0500

    bbg

--- src/pages/settings/office/courses.tsx
+++ src/pages/settings/office/courses.tsx
@@ -1,25 +1,20 @@
 //@ts-nocheck
-import {
-  Box,
-  Button,
-  Pagination,
-  Typography
-} from '@mui/material';
-import { ReactNode, useContext, useEffect } from 'react';
-import IconifyIcon from 'src/@core/components/icon';
-import DataTable from 'src/@core/components/table';
-import MuiDrawer, { DrawerProps } from '@mui/material/Drawer';
-import { styled } from '@mui/material/styles';
-import { useTranslation } from 'react-i18next';
-import { useAppDispatch, useAppSelector } from 'src/store';
-import { fetchCoursesList, setOpenCreateSms, updateParams } from 'src/store/apps/settings';
-import CourseListRowOptions from 'src/views/apps/settings/courses/CourseListRowOptions';
-import CreateCourseDialog from 'src/views/apps/settings/courses/CreateCourseDialog';
-import EditCourseDialog from 'src/views/apps/settings/courses/EditCourseDialog';
-import VideoHeader, { videoUrls } from 'src/@core/components/video-header/video-header';
-import { toast } from 'react-hot-toast';
-import { AuthContext } from 'src/context/AuthContext';
-import { useRouter } from 'next/router';
+import { Box, Button, Pagination, Typography } from '@mui/material'
+import { ReactNode, useContext, useEffect, useState } from 'react'
+import IconifyIcon from 'src/@core/components/icon'
+import DataTable from 'src/@core/components/table'
+import MuiDrawer, { DrawerProps } from '@mui/material/Drawer'
+import { styled } from '@mui/material/styles'
+import { useTranslation } from 'react-i18next'
+import { useAppDispatch, useAppSelector } from 'src/store'
+import { fetchCoursesList, setOpenCreateSms, updateParams } from 'src/store/apps/settings'
+import CourseListRowOptions from 'src/views/apps/settings/courses/CourseListRowOptions'
+import CreateCourseDialog from 'src/views/apps/settings/courses/CreateCourseDialog'
+import EditCourseDialog from 'src/views/apps/settings/courses/EditCourseDialog'
+import VideoHeader, { videoUrls } from 'src/@core/components/video-header/video-header'
+import { toast } from 'react-hot-toast'
+import { AuthContext } from 'src/context/AuthContext'
+import { useRouter } from 'next/router'
 
 export interface customTableProps {
   xs: number
@@ -42,7 +37,6 @@ export const CustomeDrawer = styled(MuiDrawer)<DrawerProps>(({ theme }) => ({
   }
 }))
 
-
 export function addPeriodToThousands(number: any) {
   const numStr = String(number)
 
@@ -59,13 +53,14 @@ export default function GroupsPage() {
   const { t } = useTranslation()
   const { push } = useRouter()
   const { user } = useContext(AuthContext)
+  const [lessonDays, setLessonDays] = useState<number>(0)
 
   const dispatch = useAppDispatch()
   const { course_list, is_pending, courseQueryParams } = useAppSelector(state => state.settings)
 
   useEffect(() => {
     if (!user?.role.includes('ceo') && !user?.role.includes('admin')) {
-      push("/")
+      push('/')
       toast.error("Sizda bu sahifaga kirish huquqi yo'q!")
     }
     const queryString = new URLSearchParams(courseQueryParams).toString()
@@ -105,10 +100,15 @@ export default function GroupsPage() {
       }
     },
     {
-      xs: 1.5,
+      xs: 1,
       title: t('Kurs davomiyligi (oy)'),
       dataIndex: 'month_duration',
       render: month => <span>{month} oy</span>
+    },
+    {
+      xs: 1.2,
+      title: t("To'lov uchun darslar soni"),
+      dataIndex: 'lesson_count'
     },
     {
       xs: 1,
@@ -141,8 +141,16 @@ export default function GroupsPage() {
           {t("Yangi kurs qo'shish")}
         </Button>
       </Box>
-      <DataTable loading={is_pending} columns={columns} data={course_list?.results} color text_color/>
-      {Math.ceil(course_list?.count / 10) > 1 && !is_pending && <Pagination defaultPage={courseQueryParams.page ? Number(courseQueryParams.page) : 1} count={Math.ceil(course_list?.count / 10)} variant="outlined" shape="rounded" onChange={(e: any, page) => handlePagination(page)} />}
+      <DataTable loading={is_pending} columns={columns} data={course_list?.results} color text_color />
+      {Math.ceil(course_list?.count / 10) > 1 && !is_pending && (
+        <Pagination
+          defaultPage={courseQueryParams.page ? Number(courseQueryParams.page) : 1}
+          count={Math.ceil(course_list?.count / 10)}
+          variant='outlined'
+          shape='rounded'
+          onChange={(e: any, page) => handlePagination(page)}
+        />
+      )}
       <CreateCourseDialog />
 
       <EditCourseDialog />
