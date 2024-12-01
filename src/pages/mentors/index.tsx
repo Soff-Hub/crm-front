4ba63ec2552b981130a8747aa1a2caed736f3763
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2024-12-01 12:03:53 +0500

    buss

--- src/pages/mentors/index.tsx
+++ src/pages/mentors/index.tsx
@@ -1,33 +1,25 @@
 //@ts-nocheck
-"use client";
-import {
-  Box,
-  Button,
-  Chip,
-  FormControlLabel,
-  Pagination,
-  Switch,
-  Typography,
-} from '@mui/material';
-import { ReactNode, useContext, useEffect } from 'react';
-import IconifyIcon from 'src/@core/components/icon';
-import { useTranslation } from 'react-i18next';
-import { useRouter } from 'next/router';
-import { useAppDispatch, useAppSelector } from 'src/store';
-import { fetchTeachersList, setOpenEdit, updateParams } from 'src/store/apps/mentors';
-import { formatCurrency } from 'src/@core/utils/format-currency';
-import { videoUrls } from 'src/@core/components/video-header/video-header';
-import dynamic from "next/dynamic";
-import useResponsive from 'src/@core/hooks/useResponsive';
-import { AuthContext } from 'src/context/AuthContext';
-import { toast } from 'react-hot-toast';
+'use client'
+import { Box, Button, Chip, FormControlLabel, Pagination, Switch, Typography } from '@mui/material'
+import { ReactNode, useContext, useEffect } from 'react'
+import IconifyIcon from 'src/@core/components/icon'
+import { useTranslation } from 'react-i18next'
+import { useRouter } from 'next/router'
+import { useAppDispatch, useAppSelector } from 'src/store'
+import { fetchTeachersList, setOpenEdit, updateParams } from 'src/store/apps/mentors'
+import { formatCurrency } from 'src/@core/utils/format-currency'
+import { videoUrls } from 'src/@core/components/video-header/video-header'
+import dynamic from 'next/dynamic'
+import useResponsive from 'src/@core/hooks/useResponsive'
+import { AuthContext } from 'src/context/AuthContext'
+import { toast } from 'react-hot-toast'
 
-const RowOptions = dynamic(() => import('src/views/apps/mentors/RowOptions'));
-const TeacherAvatar = dynamic(() => import('src/views/apps/mentors/AddMentorsModal').then(mod => mod.TeacherAvatar));
-const TeacherEditDialog = dynamic(() => import('src/views/apps/mentors/TeacherEditDialog'));
-const TeacherCreateDialog = dynamic(() => import('src/views/apps/mentors/TeacherCreateDialog'));
-const VideoHeader = dynamic(() => import('src/@core/components/video-header/video-header'));
-const DataTable = dynamic(() => import('src/@core/components/table'));
+const RowOptions = dynamic(() => import('src/views/apps/mentors/RowOptions'))
+const TeacherAvatar = dynamic(() => import('src/views/apps/mentors/AddMentorsModal').then(mod => mod.TeacherAvatar))
+const TeacherEditDialog = dynamic(() => import('src/views/apps/mentors/TeacherEditDialog'))
+const TeacherCreateDialog = dynamic(() => import('src/views/apps/mentors/TeacherCreateDialog'))
+const VideoHeader = dynamic(() => import('src/@core/components/video-header/video-header'))
+const DataTable = dynamic(() => import('src/@core/components/table'))
 
 export interface customTableProps {
   xs: number
@@ -50,63 +42,65 @@ export default function GroupsPage() {
   const columns: customTableProps[] = [
     {
       xs: 0.2,
-      title: t("ID"),
+      title: t('ID'),
       dataIndex: 'index'
     },
     {
       xs: 0.4,
       title: t('Rasm'),
       dataIndex: 'image',
-      render: (actions) => <TeacherAvatar skin='light' color={'info'} variant='rounded' sx={{ width: 33, height: 33 }}
-      >{
-          actions ? <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src={actions} alt="user" /> :
+      render: actions => (
+        <TeacherAvatar skin='light' color={'info'} variant='rounded' sx={{ width: 33, height: 33 }}>
+          {actions ? (
+            <img style={{ width: '100%', height: '100%', objectFit: 'cover' }} src={actions} alt='user' />
+          ) : (
             <IconifyIcon icon={'et:profile-male'} />
-        }
-      </TeacherAvatar>
-
+          )}
+        </TeacherAvatar>
+      )
     },
     {
       xs: 1.7,
-      title: t("first_name"),
+      title: t('first_name'),
       dataIndex: 'first_name'
     },
     {
       xs: 1.7,
-      title: t("phone"),
+      title: t('phone'),
       dataIndex: 'phone'
     },
     {
       xs: 1.7,
-      title: t("Doimiy oylik"),
+      title: t('Doimiy oylik'),
       dataIndex: 'amount',
-      render: (amount) => {
+      render: amount => {
         if (!isNaN(Number(amount))) {
-          return `${formatCurrency(amount)} UZS`;
+          return `${formatCurrency(amount)} UZS`
         } else {
-          return "*****";
+          return '*****'
         }
       }
     },
     {
       xs: 1.7,
-      title: t("Foiz ulush (%)"),
+      title: t('Foiz ulush (%)'),
       dataIndex: 'percentage',
-      render: (per) => `${per} %`
+      render: per => `${per} %`
     },
     {
       xs: 1.7,
-      title: t("birth_date"),
+      title: t('birth_date'),
       dataIndex: 'birth_date'
     },
     {
       xs: 1.7,
-      title: t("Ishga olingan sana"),
+      title: t('Ishga olingan sana'),
       dataIndex: 'activated_at'
     },
     {
       xs: 1,
       dataIndex: 'id',
-      title: "",
+      title: '',
       // title: <Button onClick={() => push("/employee-attendance")} variant='outlined'>{t("Davomat")}</Button>,
       render: actions => <RowOptions id={actions} status={queryParams?.status} />
     }
@@ -118,10 +112,14 @@ export default function GroupsPage() {
 
   useEffect(() => {
     if (!user?.role.includes('ceo') && !user?.role.includes('admin')) {
-      router.push("/")
+      router.push('/')
       toast.error('Sahifaga kirish huquqingiz yoq!')
     }
-    const queryString = new URLSearchParams({ ...queryParams, page: String(queryParams.page), status: String(queryParams.status) }).toString()
+    const queryString = new URLSearchParams({
+      ...queryParams,
+      page: String(queryParams.page),
+      status: String(queryParams.status)
+    }).toString()
     dispatch(fetchTeachersList(queryString))
 
     return () => {
@@ -136,8 +134,8 @@ export default function GroupsPage() {
   }
 
   const handleChangeStatus = async (_: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
-    dispatch(updateParams({ status: checked ? "archive" : "active", page: 1 }))
-    const queryString = new URLSearchParams({ status: checked ? "archive" : "active", page: "1" }).toString()
+    dispatch(updateParams({ status: checked ? 'archive' : 'active', page: 1 }))
+    const queryString = new URLSearchParams({ status: checked ? 'archive' : 'active', page: '1' }).toString()
     await dispatch(fetchTeachersList(queryString))
   }
 
@@ -146,13 +144,24 @@ export default function GroupsPage() {
       <VideoHeader item={videoUrls.teachers} />
       <Box
         className='groups-page-header'
-        sx={{ display: isMobile ? "grid" : 'flex', gridTemplateColumns: "1fr", gap: "10px", justifyContent: 'space-between', margin: '10px 0' }}
+        sx={{
+          display: isMobile ? 'grid' : 'flex',
+          gridTemplateColumns: '1fr',
+          gap: '10px',
+          justifyContent: 'space-between',
+          margin: '10px 0',
+          flexWrap: 'nowrap'
+        }}
         py={2}
       >
-        <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
-          <Typography variant='h5'>{t("Mentorlar")}</Typography>
-          <Chip label={`${teachersCount || 0}`} variant='outlined' color="primary" size="medium" />
-          <FormControlLabel control={<Switch onChange={handleChangeStatus} />} checked={queryParams.status == "archive"} label="Arxiv" />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+          <Typography variant='h5'>{t('Mentorlar')}</Typography>
+          <Chip label={`${teachersCount || 0}`} variant='outlined' color='primary' size='medium' />
+          <FormControlLabel
+            control={<Switch onChange={handleChangeStatus} />}
+            checked={queryParams.status == 'archive'}
+            label={t('archive')}
+          />
         </Box>
         <Button
           onClick={() => dispatch(setOpenEdit('create'))}
@@ -164,13 +173,15 @@ export default function GroupsPage() {
         </Button>
       </Box>
       <DataTable loading={isLoading} columns={columns} data={teachers} rowClick={rowClick} />
-      {Math.ceil(teachersCount / 10) > 1 && !isLoading && <Pagination
-        defaultPage={Number(queryParams.page)}
-        count={Math.ceil(teachersCount / 10)}
-        variant="outlined"
-        shape="rounded"
-        onChange={(e: any, page) => handlePagination(page)}
-      />}
+      {Math.ceil(teachersCount / 10) > 1 && !isLoading && (
+        <Pagination
+          defaultPage={Number(queryParams.page)}
+          count={Math.ceil(teachersCount / 10)}
+          variant='outlined'
+          shape='rounded'
+          onChange={(e: any, page) => handlePagination(page)}
+        />
+      )}
       <TeacherCreateDialog />
       <TeacherEditDialog />
     </div>
--- src/views/apps/lids/LidsHeader.tsx
+++ src/views/apps/lids/LidsHeader.tsx
@@ -1,56 +1,76 @@
-import { Box, Button, Switch, TextField } from '@mui/material';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import IconifyIcon from 'src/@core/components/icon';
-import useResponsive from 'src/@core/hooks/useResponsive';
-import useDebounce from 'src/hooks/useDebounce';
-import { useAppDispatch, useAppSelector } from 'src/store';
-import { fetchDepartmentList, setOpen, updateLeadParams } from 'src/store/apps/leads';
-import { Toggle } from 'rsuite';
+import { Box, Button, Switch, TextField } from '@mui/material'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import IconifyIcon from 'src/@core/components/icon'
+import useResponsive from 'src/@core/hooks/useResponsive'
+import useDebounce from 'src/hooks/useDebounce'
+import { useAppDispatch, useAppSelector } from 'src/store'
+import { fetchDepartmentList, setOpen, updateLeadParams } from 'src/store/apps/leads'
+import { Toggle } from 'rsuite'
 
 type Props = {}
 
-export default function LidsHeader({ }: Props) {
-    const { queryParams } = useAppSelector(state => state.leads)
-    const dispatch = useAppDispatch()
-    const { push } = useRouter()
-    const { t } = useTranslation()
-    const { isMobile } = useResponsive()
+export default function LidsHeader({}: Props) {
+  const { queryParams } = useAppSelector(state => state.leads)
+  const dispatch = useAppDispatch()
+  const { push } = useRouter()
+  const { t } = useTranslation()
+  const { isMobile } = useResponsive()
 
-    const [search, setSearch] = useState<string>('')
-    const searchVal = useDebounce(search, 800)
+  const [search, setSearch] = useState<string>('')
+  const searchVal = useDebounce(search, 800)
 
-    const viewArchive = (is_active: boolean) => {
-        dispatch(updateLeadParams({ is_active }))
-        dispatch(fetchDepartmentList({ ...queryParams, is_active }))
-    }
+  const viewArchive = (is_active: boolean) => {
+    dispatch(updateLeadParams({ is_active }))
+    dispatch(fetchDepartmentList({ ...queryParams, is_active }))
+  }
 
-    useEffect(() => {
-        dispatch(updateLeadParams({ search }))
-        dispatch(fetchDepartmentList({ ...queryParams, search }))
-    }, [searchVal])
+  useEffect(() => {
+    dispatch(updateLeadParams({ search }))
+    dispatch(fetchDepartmentList({ ...queryParams, search }))
+  }, [searchVal])
 
-    return (
-        <Box sx={{ width: '100%', p: '10px 0', display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-            <form style={{ display: 'flex', alignItems: 'center', gap: '5px' }} onSubmit={e => e.preventDefault()}>
-                <TextField
-                    defaultValue={''}
-                    autoComplete='off'
-                    size='small'
-                    sx={{ maxWidth: '300px', width: '100%' }} color='primary' placeholder={`${t("Qidirish")}...`} onChange={(e) => {
-                        setSearch(e.target.value)
-                    }} />
-                <label style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                    <Switch checked={!queryParams.is_active} onChange={(e, v) => viewArchive(!v)} />
-                    {t("Arxiv")}
-                </label>
-                <Button variant='outlined' onClick={() => push('/lids/stats')}>{t("Hisobot")}</Button>
-            </form>
-            {!isMobile &&
-                <Button onClick={() => dispatch(setOpen('add-department'))} sx={{ minWidth: '300px' }} size='small' variant='contained'
-                    startIcon={<IconifyIcon icon={'material-symbols:add'} />}>{t("Bo'lim yaratish")}
-                </Button>}
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        p: '10px 0',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexWrap: 'wrap'
+      }}
+    >
+      <form style={{ display: 'flex', alignItems: 'center', gap: '5px' }} onSubmit={e => e.preventDefault()}>
+        <TextField
+          defaultValue={''}
+          autoComplete='off'
+          size='small'
+          sx={{ maxWidth: '300px', width: '100%' }}
+          color='primary'
+          placeholder={`${t('Qidirish')}...`}
+          onChange={e => {
+            setSearch(e.target.value)
+          }}
+        />
+        <label style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Switch checked={!queryParams.is_active} onChange={(e, v) => viewArchive(!v)} />
+          {t('Arxiv')}
+        </label>
+        <Button variant='outlined' onClick={() => push('/lids/stats')}>
+          {t('Hisobot')}
+        </Button>
+      </form>
+      <Button
+        onClick={() => dispatch(setOpen('add-department'))}
+        sx={{ minWidth: '300px' }}
+        size='small'
+        variant='contained'
+        startIcon={<IconifyIcon icon={'material-symbols:add'} />}
+      >
+        {t("Bo'lim yaratish")}
+      </Button>
+    </Box>
+  )
 }
