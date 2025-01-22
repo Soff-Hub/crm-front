ec856cf97763e129920328bec99809e85bd90f9a
Otabek0880 <hdildora42@gmail.com>
2025-01-22 19:31:28 +0500

    students grades changes

--- src/views/apps/groups/view/GroupViewLeft/GroupDetails.tsx
+++ src/views/apps/groups/view/GroupViewLeft/GroupDetails.tsx
@@ -246,7 +246,7 @@ export default function GroupDetails() {
               </Button>
             </Tooltip>
           )}
-          {isGettingGroupDetails ? (
+          {/* {isGettingGroupDetails ? (
             <Skeleton variant='rounded' animation='wave' width={70} height={40} />
           ) : (
             <Tooltip title={t('Online dars')} placement='top'>
@@ -260,7 +260,7 @@ export default function GroupDetails() {
                 <IconifyIcon icon='mdi:laptop' />
               </Button>
             </Tooltip>
-          )}
+          )} */}
         </CardActions>
       ) : (
         ''
